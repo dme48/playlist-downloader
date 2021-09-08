681499bb5313dd9645d26f9642bb36309191780b
dme48 <dmartinezechevarria@gmail.com>
2021-09-08 10:46:32 +0200

    Added docstrings to playlist_scrapper.py

--- playlist_scrapper.py
+++ playlist_scrapper.py
@@ -1,12 +1,20 @@
+"""Container for Scrapper"""
 from urllib import request
 import re
 
 class Scrapper:
+    """Class that extracts songs from a spotify url"""
     def __init__(self, url):
+        """
+        Accesses and saves the raw html of the spotify url.
+            Parameters:
+                url (string): Spotify's playlist url.
+        """
         self.raw_html = request.urlopen(url).read().decode("utf-8")
         self.songs()
     
     def songs(self):
+        """Extracts song titles from the raw html"""
         pattern = r'(?<="is_playable":true,"name":")[^"]+(?=")'
         titles = re.findall(pattern, self.raw_html)
         return titles
