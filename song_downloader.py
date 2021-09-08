29c8f971eff85036a5e36256bde30159aa6f479f
dme48 <dmartinezechevarria@gmail.com>
2021-09-08 10:46:09 +0200

    Added docstrings to song_downloader.py

--- song_downloader.py
+++ song_downloader.py
@@ -1,9 +1,18 @@
+"""Container for the Downloader class"""
 import os
 import threading
 from youtubesearchpython import VideosSearch
 
 
 def str_to_sec(duration):
+    """
+    Converts a measure of time into its corresponding seconds
+        Parameters:
+            duration (str): The time interval. Must be specified int the format
+                (h)h:mm:ss, (m)m:ss or (s)s.
+        Returns:
+            coutn (int): Number of seconds in the interval.
+    """
     count = 0
     units = [int(s) for s in duration.split(":")]
     for unit in units:
@@ -13,9 +22,16 @@ def str_to_sec(duration):
 
 
 class Downloader:
+    """Class that handles the download of a single video"""
     TRIAL_VIDS = 3
 
     def __init__(self, title, path):
+        """
+        Querys and selects a video; sets the download as a thread.
+            Parameters:
+                title (str): title of the song
+                path (str): path of the directory where the song is to ve saved
+        """
         self.title = title
         self.path = path
         self.vid_query = VideosSearch(title, limit=Downloader.TRIAL_VIDS)
@@ -23,6 +39,11 @@ class Downloader:
         self.job = threading.Thread(target=self.call_ydl)
 
     def min_duration_video(self):
+        """
+        Once the query is done, selects the shortest video found.
+            Returns:
+                selected_vid (dict): Info on the shortest vid.
+        """
         min_duration = float("inf")
         selected_vid = None
         for vid in self.vid_query.result()["result"]:
@@ -33,9 +54,14 @@ class Downloader:
         return selected_vid
 
     def download(self):
+        """Wrapper around the thread's job"""
         self.job.start()
 
     def call_ydl(self):
+        """
+        'Call youtube-dl'. Creates the adequate shell command to download the
+        previously selected video.
+        """
         url = self.vid_info["link"]
         command = "youtube-dl -x --audio-format mp3 \"" + url + "\" -o \"" + self.path + "/%(title)s.%(ext)s\""
         print("The command is: " + command)
