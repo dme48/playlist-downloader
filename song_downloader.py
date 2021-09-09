a280db283fde6ca5cb438284ac98363cb7539c86
dme48 <dmartinezechevarria@gmail.com>
2021-09-09 23:31:46 +0200

    Introduced DownloadManager

--- song_downloader.py
+++ song_downloader.py
@@ -21,6 +21,46 @@ def str_to_sec(duration):
     return count
 
 
+class DownloadManager:
+    """Class that handles all the downloads through Downloader instances"""
+
+    def __init__(self, song_list, path):
+        """
+        Creates a Downloader instance for each song in song_list. Also initializes
+        the has_started list, which keeps track of which downloads have been started.
+            Parameters:
+                song_list (string list): list with the titles of the songs to be downloaded.
+                path (string): path of the directory where the songs will be saved.
+        """
+        self.song_list = song_list
+        self.path = path
+        self.downloads = [Downloader(song, path) for song in song_list]
+        self.has_started = [False] * len(song_list)
+
+    def print_song_id(self):
+        """Prints the song names and their indexes"""
+        for i, song in enumerate(self.song_list):
+            print("id: {},\ttitle: {}".format(i, song))
+
+    def start_all(self):
+        """Starts all downloads that haven't started already."""
+        num_songs = len(self.song_list)
+        for i in range(num_songs):
+            if self.has_started[i]:
+                continue
+            self.downloads[i].download()
+            self.has_started[i] = True
+
+    def start_by_id(self, index):
+        """Starts the download of the song with the indicated id."""
+        self.downloads[index].download()
+        self.has_started[index] = True
+
+    def start_by_name(self, song_name):
+        """Starts the download of the song with the indicated name."""
+        index = self.song_list.index(song_name)
+        self.start_by_id(index)
+
 class Downloader:
     """Class that handles the download of a single video"""
     TRIAL_VIDS = 3
