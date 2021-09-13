b0258589cf353194ac0b5989856ebe19985b03c4
dme48 <dmartinezechevarria@gmail.com>
2021-09-13 10:41:50 +0200

    Now scrap.py finds the artist names
    This new code is not yet connected to the rest of the program, as
    the artist names are never returned. Soon to be connected, possibly
    reworking scrap as a class.

--- scrap.py
+++ scrap.py
@@ -8,6 +8,40 @@ def scrap_songs(url):
         Parameters:
             url (str): spotify's playlist url.
     """
+    title_pattern = r'(?<="is_playable":true,"name":")[^"]+(?=")'
+    song_titles = re.findall(title_pattern, html)
+
+    artist_pattern = r'(?<="name":")[^"]+(?=","type":"artist")'
+    artist_names = re.findall(artist_pattern, html)
+    artist_names = purge_duplicated(artist_names)
+
+    return song_titles
+
+def purge_duplicated(duplicated_list):
+    """
+    Removes duplicated elements from a list WHILE MAINTAINING ORDER. Cannot convert from list to
+    set and back to list since it would change the order of apparition in the html.
+        Parameters:
+            duplicated_list (list): List with (possibly) repeated items
+    """
+    purged_list = []
+    found_elements = set()
+    for item in duplicated_list:
+        if item in found_elements:
+            continue
+        found_elements.add(item)
+        purged_list.append(item)
+    
+    return purged_list
+
+if __name__ == "__main__":
+    url = "https://open.spotify.com/playlist/4oJvONQDLoXaZ7TxkRUz3Q?si=d6eb4d34d95a40f6"
     html = request.urlopen(url).read().decode("utf-8")
-    pattern = r'(?<="is_playable":true,"name":")[^"]+(?=")'
-    return re.findall(pattern, html)
+
+    title_pattern = r'(?<="is_playable":true,"name":")[^"]+(?=")'
+    song_titles = re.findall(title_pattern, html)
+
+    artist_pattern = r'(?<="name":")[^"]+(?=","type":"artist")'
+    artist_names = re.findall(artist_pattern, html)
+    print(song_titles)
+    print(purge_duplicated(artist_names))
