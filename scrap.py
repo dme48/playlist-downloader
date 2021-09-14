cc8bc0a302cf2eadd7d0293908aedbcb6e0ada89
dme48 <dmartinezechevarria@gmail.com>
2021-09-14 12:00:37 +0200

    scrap_songs now returns artist names as well
    The queries will now be more specific, since previously it was
    impossible to download a cover unless it was more popular than the
    original.
    
    This being said, the contents of scrap.py are still quite messy. The
    module still needs to migrate from using regex to parsing the html;
    besides not being a very readable approach, currently duplicated
    matches have to be deleted manually (since the order of appearance
    has to be preserved). There shouldn't be duplications from the
    beginning.

--- scrap.py
+++ scrap.py
@@ -4,18 +4,29 @@ import re
 
 def scrap_songs(url):
     """
-    Shows the songs in a spotify's url.
+    Returns the songs in a spotify's url.
         Parameters:
             url (str): spotify's playlist url.
+        Returns:
+            (string list): List containing the songs, title and artist, from the url 
     """
+    html = request.urlopen(url).read().decode("utf-8")
+    
+    titles = scrap_titles(html)
+    artists = scrap_artists(html)
+
+    return ["{}, {}".format(t, a) for t, a in zip(titles, artists)]
+
+def scrap_titles(raw_html):
+    """Sraps the titles of the songs from the raw html"""
     title_pattern = r'(?<="is_playable":true,"name":")[^"]+(?=")'
-    song_titles = re.findall(title_pattern, html)
+    return re.findall(title_pattern, raw_html)
 
+def scrap_artists(raw_html):
+    """Sraps the artists of the songs from the raw html"""
     artist_pattern = r'(?<="name":")[^"]+(?=","type":"artist")'
-    artist_names = re.findall(artist_pattern, html)
-    artist_names = purge_duplicated(artist_names)
-
-    return song_titles
+    duplicated_artists = re.findall(artist_pattern, raw_html)
+    return purge_duplicated(duplicated_artists)
 
 def purge_duplicated(duplicated_list):
     """
@@ -36,12 +47,4 @@ def purge_duplicated(duplicated_list):
 
 if __name__ == "__main__":
     url = "https://open.spotify.com/playlist/4oJvONQDLoXaZ7TxkRUz3Q?si=d6eb4d34d95a40f6"
-    html = request.urlopen(url).read().decode("utf-8")
-
-    title_pattern = r'(?<="is_playable":true,"name":")[^"]+(?=")'
-    song_titles = re.findall(title_pattern, html)
-
-    artist_pattern = r'(?<="name":")[^"]+(?=","type":"artist")'
-    artist_names = re.findall(artist_pattern, html)
-    print(song_titles)
-    print(purge_duplicated(artist_names))
+    print(scrap_songs(url))
