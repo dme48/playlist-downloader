2283f03813987f7c1e99d4ca6b9bb4989442b35e
dme48 <dmartinezechevarria@gmail.com>
2021-10-22 18:18:43 +0200

    Path checking at Convertor instead of Manager

--- conversion.py
+++ conversion.py
@@ -2,11 +2,12 @@
 import os
 from pydub import AudioSegment
 
+
 class ConversionManager:
     """Manages the audio conversions through Converter instances"""
+
     def __init__(self, audio_paths: list[str]) -> None:
         """Checks the paths and creates the converters"""
-        check_paths(audio_paths)
         self.converters = [Converter(path) for path in audio_paths]
 
     def convert_all(self, new_extension) -> None:
@@ -19,10 +20,13 @@ class ConversionManager:
         for converter in self.converters:
             converter.delete_original()
 
+
 class Converter:
     """Interface to convert the format of an audio file"""
+
     def __init__(self, path: str) -> None:
         """Parses the original name and extension"""
+        check_path(path)
         self.original_path = path
         self.filename, extension = os.path.splitext(path)
         self.original_extension = extension.split(".")[1]
@@ -30,7 +34,8 @@ class Converter:
     def convert_to(self, new_extension: str) -> None:
         """Converts the original file to the new_extension format"""
         new_file = f"{self.filename}.{new_extension}"
-        original_audio = AudioSegment.from_file(self.original_path, format=self.original_extension)
+        original_audio = AudioSegment.from_file(
+            self.original_path, format=self.original_extension)
         original_audio.export(new_file, format=new_extension)
 
     def delete_original(self) -> None:
@@ -38,15 +43,11 @@ class Converter:
         os.remove(self.original_path)
 
 
-def check_paths(paths: list[str]) -> None:
-    """Calls check_path on every path in the list"""
-    for path in paths:
-        check_path(path)
-
 def check_path(path: str) -> None:
     """Checks a file exists at path."""
     if not os.path.exists(path):
         raise ValueError(f"File is not found at {path}.")
+
 
 if __name__ == "__main__":
     PATH = os.getcwd() + "/RandomFile.mp3"
