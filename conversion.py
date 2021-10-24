4687093a1d0c2e1144a95fa9265e80ea0c3c599f
dme48 <dmartinezechevarria@gmail.com>
2021-10-24 21:41:47 +0200

    implemented conversion module with Path

--- conversion.py
+++ conversion.py
@@ -1,13 +1,14 @@
 """Module for converting playlist_downloader's audio files into different formats."""
 import os
 from pydub import AudioSegment
+from pathlib import Path
 
 
 class ConversionManager:
     """Manages the audio conversions through Converter instances"""
 
-    def __init__(self, audio_paths: list[str]) -> None:
-        """Checks the paths and creates the converters"""
+    def __init__(self, audio_paths: list[Path]) -> None:
+        """Creates the converters"""
         self.converters = [Converter(path) for path in audio_paths]
 
     def convert_all(self, new_extension) -> None:
@@ -24,7 +25,7 @@ class ConversionManager:
 class Converter:
     """Interface to convert the format of an audio file"""
 
-    def __init__(self, path: str) -> None:
+    def __init__(self, path: Path) -> None:
         """Parses the original name and extension"""
         check_path(path)
         self.original_path = path
@@ -35,19 +36,20 @@ class Converter:
         """Converts the original file to the new_extension format"""
         new_file = f"{self.filename}.{new_extension}"
         original_audio = AudioSegment.from_file(
-            self.original_path, format=self.original_extension)
+            self.original_path,
+            format=self.original_extension)
         original_audio.export(new_file, format=new_extension)
 
     def delete_original(self) -> None:
         """Deletes the original file"""
         # We check in case path has been already deleted
         check_path(self.original_path)
-        os.remove(self.original_path)
+        self.original_path.unlink()
 
 
-def check_path(path: str) -> None:
+def check_path(path: Path) -> None:
     """Checks a file exists at path."""
-    if not os.path.exists(path):
+    if not path.exists():
         raise ValueError(f"File is not found at {path}.")
 
 
