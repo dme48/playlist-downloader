04a1e54a3c2144690961a63de833c1d9adae52b4
dme48 <dmartinezechevarria@gmail.com>
2021-10-28 09:01:23 +0200

    main now has a namespace as input

--- playlist_downloader.py
+++ playlist_downloader.py
@@ -2,48 +2,45 @@
 """Searches and downloads a playlist"""
 import argparse
 from pathlib import Path
+from typing import Callable
 from scrap import Scrapper
 from downloads import DownloadManager
 from conversion import ConversionManager
 
 
-def main(url: str,
-         artist: str = None,
-         path: str = None,
-         extension: str = None,
-         keep_originals: bool = False,
-         appended_songs: list[str] = None) -> None:
+def main(args: argparse.Namespace) -> None:
     """
     Downloads the songs inside a playlist.
-    Parameters:
+    Possible parameters inside args:
         url (str): url link to a Spotify's playlist. Can be obtained inside spotify's desktop
             app by right-clicking --> Share --> Copy Spotify URL.
         artist (str): if different than None, only the songs with an artist containing
             selected_artist will be downloaded (case insensitive)
         path (Path): Folder to download the songs into. If it doesn't exist it will be created.
         extension (str): Desired format of the output audios (mp3, f.x.).
+        keep_originals (bool): If True, original files will be kept after a change of format
         appended_songs (list[str]): list with searchings to be appended to the playlist songs.
     """
-    path = path if path else Path("Songs/")
+    path = args.path if args.path else Path("Songs/")
     playlist_titles = []
-    if url:
-        playlist_titles += Scrapper(url, artist).get_searchstring()
-    if appended_songs:
-        playlist_titles += appended_songs
+    if args.url:
+        playlist_titles += Scrapper(args.url, args.artist).get_searchstring()
+    if args.appended_songs:
+        playlist_titles += args.appended_songs
 
     down_manager = DownloadManager(playlist_titles, path)
     down_manager.start_all()
     down_manager.wait_until_finished()
     audio_paths = down_manager.get_file_paths()
 
-    if extension:
+    if args.extension:
         conv_manager = ConversionManager(audio_paths)
-        conv_manager.convert_all("mp3")
-        if not keep_originals:
+        conv_manager.convert_all(args.extension)
+        if not args.keep_originals:
             conv_manager.delete_originals()
 
 
-def check_arguments_are_valid(args: argparse.Namespace, parser: argparse.ArgumentParser):
+def check_arguments_are_valid(args: argparse.Namespace, send_error: Callable[[str], None]):
     """
     Checks that the arguments in args are valid, otherwise sends an error through parser.
     Checks for:
@@ -51,11 +48,11 @@ def check_arguments_are_valid(args: argparse.Namespace, parser: argparse.Argumen
         - keep_originals can only be present if another extension has been provided
     """
     if not args.url and not args.appended_songs:
-        parser.error(
-            "Either a url or a list of appended songs (--append) must be provided.")
+        send_error("Either a url or a list of appended songs \
+                    (--append) must be provided.")
     if not args.extension and args.keep_originals:
-        parser.error(
-            "Flag --keep-originals may only be passed if a change of extension (--extension) is provided.")
+        send_error("Flag --keep-originals may only be passed if a \
+                    change of extension (--extension) is provided.")
 
 
 if __name__ == "__main__":
@@ -86,12 +83,6 @@ if __name__ == "__main__":
                         help="List of songs to append to include in the download.")
 
     parsed_args = parser.parse_args()
-    check_arguments_are_valid(parsed_args, parser)
+    check_arguments_are_valid(parsed_args, parser.error)
 
-    main(parsed_args.url,
-         artist=parsed_args.artist,
-         path=parsed_args.path,
-         extension=parsed_args.extension,
-         keep_originals=parsed_args.keep_originals,
-         appended_songs=parsed_args.appended_songs
-         )
+    main(parsed_args)
